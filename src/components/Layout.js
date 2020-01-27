f130f4af79c453b250cc3ff92955ac14c11699aa
itsaiub <itsaiub@gmail.com>
2020-01-27 23:54:49 +0600

    Navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,8 @@ import React from "react"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
 
+import "./layout.css"
+
 const Layout = ({ children }) => {
   return (
     <>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,60 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
+import { FaAlignRight } from "react-icons/fa"
+
+import styles from "../css/navbar.module.css"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
 
 const NavBar = () => {
+  const [isOpenNav, setIsOpenNav] = useState(false)
+
+  const toggleNav = () => {
+    setIsOpenNav(isOpenNav => !isOpenNav)
+  }
+
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/contact/">Contact</Link>
-      <Link to="/tours/">Tours</Link>
-      <Link to="/blog/">Blog</Link>
-    </div>
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <h1 className={styles.logo}>
+            Tour<span style={{ color: "#3fd0d4" }}>Guide</span>
+          </h1>
+          <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+            <FaAlignRight className={styles.logoIcon} />
+          </button>
+        </div>
+        <ul
+          className={
+            isOpenNav
+              ? `${styles.navLinks} ${styles.showNav}`
+              : `${styles.navLinks}`
+          }
+        >
+          {links.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={item.path}>{item.text.toUpperCase()}</Link>
+              </li>
+            )
+          })}
+        </ul>
+        <div className={styles.navSocialLinks}>
+          {socialIcons.map((item, index) => {
+            return (
+              <a
+                key={index}
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.icon}
+              </a>
+            )
+          })}
+        </div>
+      </div>
+    </nav>
   )
 }
 
