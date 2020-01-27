eb24837d560ad60865c2491e4fd43c739ea45a8c
itsaiub <itsaiub@gmail.com>
2020-01-28 00:59:21 +0600

    error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { Link } from "gatsby"
 
+import styles from "../css/error.module.css"
 import Layout from "../components/Layout"
+import Banner from "../components/Banner"
 
 const error = () => {
   return (
@@ -10,7 +13,13 @@ const error = () => {
         <meta charSet="utf-8" />
         <title>Page Not Found - TourGuide</title>
       </Helmet>
-      <h1>Error</h1>
+      <header className={styles.error}>
+        <Banner title="oops it's a dead end">
+          <Link to="/" className="btn-white">
+            Back to Homepage
+          </Link>
+        </Banner>
+      </header>
     </Layout>
   )
 }
