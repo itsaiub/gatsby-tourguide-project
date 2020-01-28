1319baddce875f9821a12a284ae43cc138863a44
itsaiub <itsaiub@gmail.com>
2020-01-29 00:07:45 +0600

    styled hero with background plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
-import SimpleHero from "../components/SimpleHero"
+import StyledHero from "../components/StyledHero"
 import Banner from "../components/Banner"
-import { Link } from "gatsby"
 import About from "../components/Home/About"
 import Services from "../components/Home/Services"
 
-export default () => (
+export default ({ data: { defaultBcg } }) => (
   <Layout>
     <Helmet>
       <meta charSet="utf-8" />
       <title>Home - TourGuide</title>
     </Helmet>
-    <SimpleHero>
+    <StyledHero home="true" img={defaultBcg.childImageSharp.fluid}>
       <Banner
         title="continue exploring"
         info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, consectetur!"
@@ -23,8 +23,20 @@ export default () => (
           explore tours
         </Link>
       </Banner>
-    </SimpleHero>
+    </StyledHero>
     <About />
     <Services />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 4160, quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
